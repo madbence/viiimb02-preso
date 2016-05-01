85c55a2df03bd9471e274b904803ca787399f8e2
Bence Dányi <bence@danyi.me>
2016-05-02 00:37:19 +0200

    fix indent

--- host-pseudo.c
+++ host-pseudo.c
@@ -4,11 +4,11 @@
 #include <curand.h>
 
 #define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
-    printf("Error at %s:%d\n",__FILE__,__LINE__);\
-    return EXIT_FAILURE;}} while(0)
+  printf("Error at %s:%d\n",__FILE__,__LINE__);\
+  return EXIT_FAILURE;}} while(0)
 #define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
-    printf("Error at %s:%d\n",__FILE__,__LINE__);\
-    return EXIT_FAILURE;}} while(0)
+  printf("Error at %s:%d\n",__FILE__,__LINE__);\
+  return EXIT_FAILURE;}} while(0)
 
 int main(int argc, char *argv[])
 {
